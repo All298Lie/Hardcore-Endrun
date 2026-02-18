593ac0bd41ce41eb7a66868f65d00fa31ef59271
All298Lie <whdudfo333@naver.com>
2026-02-19 00:55:01 +0900

    init commit

---
+++ src/main/java/dev/all298lie/hcr.java
@@ -0,0 +1,17 @@
+package dev.all298lie;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class hcr extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
